ad863918542f0c61081e1256e35824666eaa6046
ozgur <ozgurpir14@gmail.com>
2020-02-18 22:17:37 +0300

    minor change

--- src/main/java/com/daimontech/dsapi/controller/AuthRestAPIs.java
+++ src/main/java/com/daimontech/dsapi/controller/AuthRestAPIs.java
@@ -105,18 +105,18 @@ public class AuthRestAPIs {
 	    			Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
 	                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
 	    			roles.add(adminRole);
-	    			
+
 	    			break;
 	    		case "pm":
 	            	Role pmRole = roleRepository.findByName(RoleName.ROLE_PM)
 	                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
 	            	roles.add(pmRole);
-	            	
+
 	    			break;
 	    		default:
 	        		Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
 	                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
-	        		roles.add(userRole);        			
+	        		roles.add(userRole);
         	}
         });*/
         Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
