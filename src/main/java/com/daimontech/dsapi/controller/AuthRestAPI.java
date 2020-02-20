d9c13994826d2752335ac12db2d3fe2f5789ac30
ozgur <ozgurpir14@gmail.com>
2020-02-20 09:50:56 +0300

    api documantation was added

--- src/main/java/com/daimontech/dsapi/controller/AuthRestAPI.java
+++ src/main/java/com/daimontech/dsapi/controller/AuthRestAPI.java
@@ -34,7 +34,7 @@ import java.util.Set;
 @RestController
 @RequestMapping("/api/auth")
 @Api(value="Auth islemleri")
-public class AuthRestAPIs {
+public class AuthRestAPI {
 
     @Autowired
     AuthenticationManager authenticationManager;
