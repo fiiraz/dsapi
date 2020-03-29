a0492cbe7ec87c8ffe50f130789ec4a8a223745d
aykut <aykut@apoblo.com>
2020-03-29 16:25:34 +0300

    tables were updated

--- src/main/java/com/daimontech/dsapi/photos/model/RecommendedPhotos.java
+++ src/main/java/com/daimontech/dsapi/photos/model/RecommendedPhotos.java
@@ -1,13 +1,12 @@
 package com.daimontech.dsapi.photos.model;
 
-import com.daimontech.dsapi.model.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import javax.persistence.*;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.NotBlank;
 
 
 @Getter
@@ -15,29 +14,19 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "recommended_new_packages_rate ")
 //urun sahibinin musteriye onermeyi planladigi yeni urunlerin puanlamasi
-public class RecommendedNewPackagesRate {  // bu tabloyu yorumu musteriden almak icin olusturduk
+public class RecommendedPhotos {  // bu tabloyu yorumu musteriden almak icin olusturduk
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(min = 1, max = 5)
-    private int rate;
-
-    @Size(min = 3, max = 50)
-    private String comment;
+    @NotBlank
+    private String photos;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "recommended_new_packages_id", nullable = false)
+    @JoinColumn(name = "recommend_product_rate_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
-    private RecommendedNewPackages recommendedNewPackages;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JsonIgnore
-    private User user;
-
+    private RecommendedProductRate recommendedProductRate;
 
 
 }
