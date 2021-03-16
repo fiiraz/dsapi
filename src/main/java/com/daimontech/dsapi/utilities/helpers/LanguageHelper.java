c63138043a6ab2f6a0ddaa76738c6a7bb4ac3725
ozgur <ozgurpir14@gmail.com>
2021-03-16 20:17:33 +0300

    rate bug fixed and language started

--- src/main/java/com/daimontech/dsapi/utilities/helpers/LanguageHelper.java
+++ src/main/java/com/daimontech/dsapi/utilities/helpers/LanguageHelper.java
@@ -1,8 +1,12 @@
 package com.daimontech.dsapi.utilities.helpers;
 
+import com.daimontech.dsapi.model.User;
 import com.daimontech.dsapi.utilities.error.ErrorMessagesEn;
 import com.daimontech.dsapi.utilities.error.ErrorMessagesTr;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +19,7 @@ public class LanguageHelper {
     ErrorMessagesEn errorMessagesEn;
 
     public void language(String lang) {
+        System.out.println("LANG: " + lang);
         if (lang.equals("tr")) {
             errorMessagesTr.setErrorMap();
         }
